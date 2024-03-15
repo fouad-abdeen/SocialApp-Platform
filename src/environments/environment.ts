505ad89d76ccbb325aaf0321f3e980124e6018b8
Fouad Abdeen <abdeen.dev@gmail.com>
2024-03-15 21:16:21 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:3030',
+  // Temporarily use the same server url as the production environment.
+  serverUrl: 'https://socialapp-server-x77i.onrender.com',
+  // When running the server locally, use the following url: 'http://localhost:3030'
 };
